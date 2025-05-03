cd648cacee77bd91ba08bdf06b504b3c5c2d0b40
GamerCleanVic <jonassd2000@gmail.com>
2025-05-03 00:40:56 -0500

    no release

--- olaMundo/exercise1/target/release/exercise1.d
+++
@@ -1,1 +0,0 @@
-/home/std/Documentos/onlineEstudos/comprehensive-rust/olaMundo/exercise1/target/release/exercise1: /home/std/Documentos/onlineEstudos/comprehensive-rust/olaMundo/exercise1/src/main.rs
